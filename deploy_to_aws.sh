0abd9f0e46b9cbe57ab7776013223f861238a58e
Daniel Johannessen <daniel.johannessen@sesam.io>
2021-03-08 19:52:35 +0100

    aws autodeploy

---
+++ deploy_to_aws.sh
@@ -0,0 +1,9 @@
+echo 'Starting to Deploy...'
+ssh ubuntu@52.38.40.75 " sudo docker image prune -f 
+        cd tictactoe_rails_react 
+        sudo docker-compose down
+        git fetch origin
+        git reset --hard origin/develop  &&  echo 'You are doing well'
+        sudo docker-compose build && sudo docker-compose up -d
+        "
+echo 'Deployment completed successfully'
