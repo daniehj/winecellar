381ef1f6f9aa0a6984472bde33ced1b54b549d02
Daniel Johannessen <daniel.johannessen@sesam.io>
2021-03-08 20:34:05 +0100

    ip

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Starting to Deploy...'
-ssh ec2-user@ip-172-31-47-89 " sudo docker image prune -f
+ssh ec2-user@172.31.47.89 " sudo docker image prune -f
         cd PROG/winecellar/API
         chmode 777 deploy_to_aws.sh
         sudo docker-compose down
